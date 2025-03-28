147dd2e54d299d0e5c7e5e4cd358c341d5e7aa75
Ekaterina <e.fayzulina@inbox.ru>
2025-03-29 00:08:41 +0700

    Создание каркаса Spring Boot приложения Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import ru.yandex.practicum.filmorate.annotations.MinDate;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
+    @Positive
+    private Long id;
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+    @Length(max = 200, message = "Длина описания должна быть не больше 200 символов")
+    private String description;
+    @MinDate(date = "1895-12-28", message = "Дата релиза фильма — не раньше 28 декабря 1895 года")
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private Integer duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Positive
+    private Long id;
+    private String name;
+    @NotBlank(message = "Логин не должен быть пустым и содержать пробелов")
+    private String login;
+    @Email
+    @NotEmpty(message = "Электронная почта не может быть пустой")
+    private String email;
+    @PastOrPresent(message = "День рождение не может быть в будущем")
+    private LocalDate birthday;
+
+    public void setName(String name) {
+        this.name = (name == null || name.isBlank()) ? this.login : name;
+    }
+}
