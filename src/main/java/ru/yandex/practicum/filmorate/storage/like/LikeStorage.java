d87d40b4345412b67c3824046ad65b363d4c6901
Ekaterina <e.fayzulina@inbox.ru>
2025-05-31 19:08:40 +0700

    Спринт 12

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.like;
 
 import ru.yandex.practicum.filmorate.dto.FilmShortInfoDto;
-import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
 
